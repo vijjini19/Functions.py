5523422478990d62ef165e02d9ea0e090d3c8c84
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-26 20:26:22 +0530

    Add files via upload

---
+++ Func_program.py
@@ -0,0 +1,38 @@
+# def greet():
+#     print("Hello")
+# greet()  #Hello
+
+# def greet(n):
+#     print("Hello "+n)
+# greet("sindhu")  #Hello sindhu
+# greet("vujjini") #Hello vujjini
+
+# def greet(*names):
+#     print("Hey",names[0])
+# greet("python","programming","core","Functions")  #Hey python
+
+# def details(n,a):
+#     print('Hey '+n +'! Your age is '+str(a))
+# details('sindhu',23)      #Hey sindhu! Your age is 23
+# details(n='sindhu',a=23)  #Hey sindhu! Your age is 23
+# details(a=23,n='sindhu')  #Hey sindhu! Your age is 23
+
+# def details(n,a):
+#     print('Hey '+n + '! Your age is '+str(a))
+# n=input('Enter your name: ')
+# a=input('Enter your age: ')
+# details(n,a)                   #Hey sindhu! Your age is 23
+
+# def r(x,y):
+#     return x+y
+# print(r(5,6))  #11
+
+# def z(p,q):
+#     return p*q,p+q;
+# p=int(input())
+# q=int(input())
+# print(z(p,q))
+
+
+
+
---
+++ Functions_part1.py
@@ -0,0 +1,25 @@
+'''Function representation --> def
+Function will be executed only after calling
+A Function calling itself repeatedly is called as Recursion
+'''
+# def hello():
+#     print('Today is Tuesday')
+# hello()
+
+# def hello(n):
+#     print("My name is "+n)
+# hello("sindhu")
+
+# def hello(fname,lname):
+#     print("My name is ",fname,"and last name is ",lname)
+# hello("sindhu","vujjini")
+
+# def hi(h):
+#     print("Function topic",h)
+# hi("python")
+# hi("advance python")
+
+# def mobile(*m):
+#     print("mobile brand",m)
+# mobile("redmi","celkon","nokia","lenevo")
+# mobile(["redmi","celkon","nokia","lenevo"])
