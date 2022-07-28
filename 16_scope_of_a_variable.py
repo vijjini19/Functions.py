dafa5b3cbe73cf9e0c02c9748ba2de14c7447220
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-28 19:17:55 +0530

    Add files via upload

---
+++ 16_scope_of_a_variable.py
@@ -0,0 +1,21 @@
+'''Local Scope
+Value can be accessed inside the function only
+'''
+
+'''
+def fun():
+    x=2;y=6;z=6
+    print(x+y)
+fun()
+'''
+
+''' Global Scope of a variable can be accessed by anyone'''
+# a=2
+# def bm():
+#     a=4
+#     b=3;c=8
+#     print(a)
+#     print(b)
+# print(a)
+# bm()
+# print(a)
