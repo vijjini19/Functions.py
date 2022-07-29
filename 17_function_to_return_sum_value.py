03f16cb5f6a8e333fc078d4dcf5a58b00e597e34
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-29 21:21:14 +0530

    Add files via upload

---
+++ 17_function_to_return_sum_value.py
@@ -0,0 +1,7 @@
+def sum():
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    c=a+b
+    return c
+    # return "The value of {}+{} is {}".format(a,b,c)
+print(sum())
